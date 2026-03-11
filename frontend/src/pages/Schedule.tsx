a4ee91e2064ed07c8fde23e9b068f8d75e7200d7
dhanith-bandara <dhanith.20233048@iit.ac.lk>
2026-03-11 11:36:35 +0530

    add translation utility

--- frontend/src/pages/Schedule.tsx
+++ frontend/src/pages/Schedule.tsx
@@ -4,3 +4,4 @@ import { SearchIcon, CalendarIcon } from 'lucide-react';
 import { ScheduleCard } from '../components/ScheduleCard';
 import { mockSchedules, ScheduleItem } from '../utils/mockData';
 import { useLanguage } from '../contexts/LanguageContext';
+import { translate } from '../utils/translations';
